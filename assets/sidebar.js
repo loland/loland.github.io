2ecee53ebfb446fefc2eb61d202a7566db5d7d25
loland <73586395+loland@users.noreply.github.com>
2023-08-23 00:55:42 +0800

    hides sidebar immediately if screen small

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -1,5 +1,5 @@
 const arrows = document.querySelectorAll(".arrow");
-let body = document.getElementsByClassName('page-content')[0].getElementsByClassName('wrapper')[0];
+let page_body = document.getElementsByClassName('page-content')[0].getElementsByClassName('wrapper')[0];
 
 arrows.forEach((arrow) => {
 arrow.addEventListener("click", (e) => {
@@ -15,16 +15,29 @@ const sidebarBtn = document.querySelector(".bx-menu");
 //     sidebar.classList.toggle("close");
 // });
 
+function initial_adjust_sidebar() {
+    let sidebar_width = 260; 
+    let page_body_x = page_body.getBoundingClientRect().x;
+    if (page_body_x <= sidebar_width) {
+        sidebar.classList.add("sidebar-hidden");
+    }
+}
+
+initial_adjust_sidebar();
+
 function adjust_sidebar() {
     let sidebar_width = 260; 
-    let body_x = body.getBoundingClientRect().x;
+    let page_body_x = page_body.getBoundingClientRect().x;
     let is_closed = sidebar.classList.contains('close');
-    if (body_x <= sidebar_width && !is_closed) {
+
+    sidebar.classList.remove("sidebar-hidden");
+    if (page_body_x <= sidebar_width && !is_closed) {
         sidebar.classList.toggle('close');
-    } else if (body_x > sidebar_width && is_closed) {
+    } else if (page_body_x > sidebar_width && is_closed) {
         sidebar.classList.toggle('close');
     }
 }
 
 window.addEventListener("resize", adjust_sidebar);
-adjust_sidebar();
+
+
